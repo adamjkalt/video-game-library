de6238e4749373fc847215005e0832056ddcc949
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-08 17:50:59 +0000

    slug methods

--- app/models/console.rb
+++ app/models/console.rb
@@ -2,4 +2,13 @@ class Console < ActiveRecord::Base
 
   has_many :games
 
+
+  def slug
+  name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Console.all.find { |console| console.slug == slug}
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,12 @@ class Game < ActiveRecord::Base
 
   belongs_to :console
 
+  def slug
+  name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Game.all.find { |game| game.slug == slug}
+  end
+
 end
