5931fcf394f730ca2247d2542433e815623d8909
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-08 19:12:37 +0000

    game and console controllers

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,3 +1,13 @@
 class ConsolesController < ApplicationController
 
+  get '/consoles' do
+    @consoles = Console.all.sort_by!{ |console| console.name.downcase }
+     erb :'/consoles/index'
+   end
+
+   get '/consoles/:slug' do
+     @console = Console.find_by_slug(params[:slug])
+     erb:'/consoles/show'
+   end
+
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,44 @@
+require 'rack-flash'
+
 class GamesController < ApplicationController
+  use Rack::Flash
+
+
+  get '/games/new' do
+      erb :'/games/new'
+    end
+
+    get '/games/:slug' do
+      @game = Game.find_by_slug(params[:slug])
+      erb:'/games/show'
+    end
+
+  post '/games' do
+  @game = Game.create(name: params["Name"])
+  @game.console = Game.find_or_create_by(name: params["Console Name"])
+  @game.save
+  flash[:message] = "Successfully created game."
+  redirect("/games/#{@game.slug}")
+  end
+
+
+  get '/games/:slug/edit' do
+      @game = Game.find_by_slug(params[:slug])
+      erb :'games/edit'
+    end
+
+    patch '/games/:slug' do
+      @game = Game.find_by_slug(params[:slug])
+      @game.update(params[:game])
+      @game.console = Console.find_or_create_by(name: params[:console][:name])
+      @game.save
+      flash[:message] = "Successfully updated game."
+      redirect("/games/#{@game.slug}")
+    end
+
+    get '/games' do
+      @games = Game.all.sort_by!{ |game| game.name.downcase }
+      erb :'/games/index'
+    end
 
 end
